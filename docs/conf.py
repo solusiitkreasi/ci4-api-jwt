aa8d3b287e5fa164ebae55b54437440fd2274c48
Aryatama <18211338+aryatama045@users.noreply.github.com>
2025-07-01 09:25:07 +0700

    Update conf.py

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'ci4-api-jwt'
+copyright = '2025, solusiitkreasi'
+author = 'solusiitkreasi'
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
